077a082dca82e96f74ae7ba2626ab8776a04a5c3
umatare5 <umatare5@gmail.com>
2025-08-24 21:17:49 +0900

    feat(service): remove rrm service files
    - Remove rrm/doc.go, rrm/service.go, rrm/service_test.go
    - RRM functionality moved to service/rrm/
    - Maintains clean separation between service packages

--- rrm/doc.go
+++
@@ -1,5 +0,0 @@
-// Package rrm exposes Radio Resource Management endpoints.
-//
-// RRM provides methods to access radio frequency management data,
-// RF optimization settings, and operational statistics from Cisco Catalyst 9800 controllers.
-package rrm
