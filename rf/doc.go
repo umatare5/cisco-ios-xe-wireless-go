5a9e8239fe6f7e894bb445db12d0530653e27b0a
umatare5 <umatare5@gmail.com>
2025-08-24 21:17:28 +0900

    feat(service): remove rf service files
    - Remove rf/doc.go, rf/service.go, rf/service_test.go
    - RF functionality moved to service/rf/
    - Maintains clean separation between service packages

--- rf/doc.go
+++
@@ -1,5 +0,0 @@
-// Package rf exposes Radio Frequency management configuration endpoints.
-//
-// RF provides methods to manage RF profiles, power settings, channel assignments,
-// and spectrum management from Cisco Catalyst 9800 controllers.
-package rf
